7b8d7f2db5bca9f245fbfd863b09dbe93b1d6ab9
Meph1sto666 <phentragon@gmx.de>
2025-10-09 01:41:47 +0200

    misc: dependencies for shared and added Display for ipc::Response

--- shared/src/ipc/mod.rs
+++ shared/src/ipc/mod.rs
@@ -6,3 +6,12 @@ pub enum Response {
     Success(String),
     Error(String),
 }
+
+impl std::fmt::Display for Response {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::Success(m) => write!(f, "{}", m),
+            Self::Error(m) => write!(f, "{}", m),
+        }
+    }
+}
