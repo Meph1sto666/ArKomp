05cbce5f0cd1a551385740cf4d9795d7f40a1760
Meph1sto666 <phentragon@gmx.de>
2025-10-18 01:36:07 +0200

    refactoring: improved error handling and refactor to allow non skeleton plugins

--- shared/src/operator.rs
+++ shared/src/operator.rs
@@ -6,3 +6,23 @@ pub trait Operator: std::fmt::Debug + Send + Sync {
     fn load_textures(&mut self, ctx: &eframe::egui::Context);
     fn event_handler(&mut self, event: crate::events::Event);
 }
+
+#[derive(Debug)]
+#[non_exhaustive]
+pub enum Error {
+    SkinError(crate::skin::Error),
+}
+
+impl From<crate::skin::Error> for Error {
+    fn from(value: crate::skin::Error) -> Self {
+        Self::SkinError(value)
+    }
+}
+
+impl std::fmt::Display for Error {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Error::SkinError(error) => write!(f, "{}", error),
+        }
+    }
+}
--- shared/src/skin.rs
+++ shared/src/skin.rs
@@ -1,23 +1,34 @@
+use std::path::PathBuf;
 use eframe::egui;
-use rusty_spine::controller::SkeletonController;
+use serde::{Deserialize, Serialize};
 
 pub trait OperatorSkin: std::fmt::Debug {
-    fn new(
-        operator_id: &str,
-        skin_id: &str,
-        atlas_path: String,
-        skeleton_file: SkeletonFile,
-        texture: crate::texture::SpineTexture,
-    ) -> Self
+    fn new(operator_id: &str, skin_id: Option<&str>) -> Result<Self, Error>
     where
         Self: Sized;
     fn operator_id(&self) -> String;
-    fn id(&self) -> String;
-    fn controller_mut(&mut self) -> &mut SkeletonController;
+    fn id(&self) -> Option<String>;
     fn ensure_textures_loaded(&mut self, ctx: &egui::Context);
 }
 
 pub enum SkeletonFile {
-    Binary(String),
-    Json(String),
+    Binary(PathBuf),
+    Json(PathBuf),
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub enum Error {
+    AtlasFileNotFound(String),
+    SkeletonFileNotFound(String),
+    TextureFileNotFound(String),
+}
+
+impl std::fmt::Display for Error {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Error::AtlasFileNotFound(e) => write!(f, "Atlas error: {}", e),
+            Error::SkeletonFileNotFound(e) => write!(f, "Skeleton error: {}", e),
+            Error::TextureFileNotFound(e) => write!(f, "Texture error: {}", e),
+        }
+    }
 }
