7280e55c8ff84972038fb135d0323e6ec0ff29fa
Meph1sto666 <phentragon@gmx.de>
2025-10-29 14:39:24 +0100

    refactor: refactor IPC handling to allow for intern events and event responses

--- src/ipc_handler.rs
+++ src/ipc_handler.rs
@@ -1,146 +1,114 @@
-use futures::{SinkExt, StreamExt as _};
+use futures::{SinkExt, StreamExt};
+use serde_json::json;
 use shared::{
     events::Event,
     ipc::{Response, commands::Command},
     operator::Operator,
     plugin::PluginRegistry,
 };
-use std::{
-    collections::HashMap,
-    sync::{Arc, RwLock, mpsc::Sender},
-};
-use tokio::net::TcpListener;
-use tokio_tungstenite::tungstenite::Message;
-use tracing::{debug, error};
+use std::{collections::HashMap, sync::Arc};
+use tokio::net::{TcpListener, TcpStream};
+use tokio_tungstenite::{accept_async, tungstenite::Message};
+use tracing::{error, info};
+
+type OperatorRegistry = Arc<std::sync::RwLock<HashMap<String, Box<dyn Operator>>>>;
 
 #[derive(Debug, Clone)]
 pub struct WebSocketServer {
-    plugin_registry: Arc<RwLock<PluginRegistry>>,
-    operator_registry: Arc<RwLock<HashMap<String, Box<dyn Operator>>>>,
+    plugins: Arc<std::sync::RwLock<PluginRegistry>>,
+    operators: OperatorRegistry,
+    event_tx: tokio::sync::mpsc::Sender<Event>,
 }
 
 impl WebSocketServer {
     pub fn new(
-        plugin_registry: &Arc<std::sync::RwLock<PluginRegistry>>,
-        operator_registry: &Arc<RwLock<HashMap<String, Box<dyn Operator>>>>,
+        plugins: &Arc<std::sync::RwLock<PluginRegistry>>,
+        operators: &OperatorRegistry,
     ) -> Self {
+        let (event_tx, event_rx) = tokio::sync::mpsc::channel::<Event>(100);
+
+        let operators_clone = operators.clone();
+        tokio::spawn(Self::event_processor(event_rx, operators_clone));
+
         Self {
-            plugin_registry: plugin_registry.clone(),
-            operator_registry: operator_registry.clone(),
+            plugins: plugins.clone(),
+            operators: operators.clone(),
+            event_tx,
         }
     }
 
-    pub async fn run(&self, address: &str) -> Result<(), Box<dyn std::error::Error>> {
-        let listener = TcpListener::bind(address).await?;
-        tracing::info!("WebSocket server running on ws://{}", address);
-        let (operator_tx, service_rx) = std::sync::mpsc::channel::<Event>();
-
-        let op_reg = self.operator_registry.clone();
-        tokio::spawn(async move {
-            while let Ok(event) = service_rx.recv() {
-                handle_event(event, op_reg.clone());
-            }
-        });
+    pub async fn run(&self, addr: &str) -> Result<(), Box<dyn std::error::Error>> {
+        let listener = TcpListener::bind(addr).await?;
+        info!("WebSocket server running on ws://{}", addr);
 
         while let Ok((stream, _)) = listener.accept().await {
             let server = self.clone();
-            let op_tx = operator_tx.clone();
             tokio::spawn(async move {
-                if let Err(e) = server.handle_connection(stream, op_tx).await {
-                    error!("Connection handler error: {}", e);
+                if let Err(e) = server.handle_client(stream).await {
+                    error!("Client error: {}", e);
                 }
             });
         }
         Ok(())
     }
 
-    async fn handle_connection(
-        &self,
-        stream: tokio::net::TcpStream,
-        operator_tx: Sender<Event>,
-    ) -> Result<(), Box<dyn std::error::Error>> {
-        let ws_stream = tokio_tungstenite::accept_async(stream).await?;
-        let (mut ws_sender, mut ws_receiver) = ws_stream.split();
+    async fn handle_client(&self, stream: TcpStream) -> Result<(), Box<dyn std::error::Error>> {
+        let ws = accept_async(stream).await?;
+        let (mut writer, mut reader) = ws.split();
+        let (tx, mut rx) = tokio::sync::mpsc::unbounded_channel();
 
-        while let Some(message) = ws_receiver.next().await {
-            self.process_message(message, &mut ws_sender, operator_tx.clone())
-                .await?;
-        }
-        Ok(())
-    }
-
-    async fn process_message(
-        &self,
-        message: Result<Message, tokio_tungstenite::tungstenite::Error>,
-        ws_sender: &mut futures_util::stream::SplitSink<
-            tokio_tungstenite::WebSocketStream<tokio::net::TcpStream>,
-            Message,
-        >,
-        operator_tx: Sender<Event>,
-    ) -> Result<(), Box<dyn std::error::Error>> {
-        match message {
-            Ok(Message::Text(command_json)) => {
-                let response = self.execute_command(&command_json, operator_tx).await;
-                self.send_response(ws_sender, response).await?;
+        let write_handle = tokio::spawn(async move {
+            while let Some(response) = rx.recv().await {
+                if let Ok(json) = serde_json::to_string(&response) {
+                    if writer.send(Message::Text(json.into())).await.is_err() {
+                        break;
+                    }
+                }
             }
-            Ok(Message::Close(_)) => {
-                error!("Connection closed by client");
-                return Err("Connection closed by client".into());
-            }
-            Err(e) => {
-                error!("Error receiving message: {}", e);
-                return Err(e.into());
-            }
-            _ => {}
-        }
-        Ok(())
-    }
-
-    async fn send_response(
-        &self,
-        ws_sender: &mut futures_util::stream::SplitSink<
-            tokio_tungstenite::WebSocketStream<tokio::net::TcpStream>,
-            Message,
-        >,
-        response: Response,
-    ) -> Result<(), Box<dyn std::error::Error>> {
-        let response_json = serde_json::to_string(&response).unwrap_or_else(|_| {
-            debug!("serialisation failed: {response:?}");
-            "{\"error\":\"Serialisation failed\"}".to_string()
         });
 
-        ws_sender
-            .send(Message::Text(response_json.into()))
-            .await
-            .map_err(|e| {
-                error!("Error sending response: {}", e);
-                e
-            })?;
+        while let Some(Ok(Message::Text(cmd))) = reader.next().await {
+            let response = self.process_command(&cmd).await;
+            if tx.send(response).is_err() {
+                break;
+            }
+        }
+
+        drop(tx);
+        let _ = write_handle.await;
         Ok(())
     }
 
-    async fn execute_command(&self, command_json: &str, operator_tx: Sender<Event>) -> Response {
+    async fn event_processor(
+        mut event_rx: tokio::sync::mpsc::Receiver<Event>,
+        operators: OperatorRegistry,
+    ) {
+        while let Some(event) = event_rx.recv().await {
+            if let Some(op) = operators.write().unwrap().get_mut(event.operator_id()) {
+                if let Ok(_response) = op.event_handler(event) {                    
+                    // let _ = event_tx.send(response);
+                    // just ignore channel sent events responses for now...
+                }
+            }
+        }
+    }
+
+    async fn process_command(&self, cmd: &str) -> Response {
         match Command::execute_from_json(
-            command_json,
+            cmd,
             &mut shared::ipc::command_context::CommandContext::new(
-                self.operator_registry.clone(),
-                self.plugin_registry.clone(),
-                operator_tx,
+                self.operators.clone(),
+                self.plugins.clone(),
+                self.event_tx.clone(),
             ),
         ) {
             Ok(response) => response,
-            Err(e) => Response::Error(format!("Command execution failed: {:?}", e)),
+            Err(e) => Response::Error(
+                json!({
+                    "reason": e
+                })
+                .to_string(),
+            ),
         }
     }
-}
-
-fn handle_event(event: Event, op_registry: Arc<RwLock<HashMap<String, Box<dyn Operator>>>>) {
-    if let Some(op) = op_registry.write().unwrap().get_mut(event.operator_id()) {
-        op.event_handler(event)
-    } else {
-        debug!(
-            "Failed to designate event {:?}; Operator not in registry",
-            event
-        )
-    };
 }
