a4edc3eaab99026f2a7ed618daad7ad5a26ab973
Meph1sto666 <phentragon@gmx.de>
2025-10-30 15:11:53 +0100

    feat!: ffi safe arguments for plugins

--- shared/src/plugin/types/operator_plugin.rs
+++ shared/src/plugin/types/operator_plugin.rs
@@ -1,10 +1,10 @@
-use tracing::{debug, error};
-
 use crate::{
+    events::Event,
     operator::Operator,
     plugin::{Error, Plugin, PluginLibrary},
 };
-use std::path::Path;
+use std::{ffi::CString, path::Path};
+use tracing::{debug, error};
 
 #[derive(Debug)]
 pub struct OperatorPlugin {
@@ -29,9 +29,16 @@ impl OperatorPlugin {
         Ok(Self { library, name })
     }
 
-    pub fn build(&self, id: Option<String>) -> Result<Box<dyn Operator>, Error> {
+    pub fn build(
+        &self,
+        id: Option<String>,
+        event_tx: tokio::sync::mpsc::Sender<Event>,
+    ) -> Result<Box<dyn Operator>, Error> {
         let constructor: libloading::Symbol<
-            fn(Option<String>) -> Result<Box<dyn Operator>, crate::operator::Error>,
+            fn(
+                *const std::ffi::c_char,
+                *const std::ffi::c_void,
+            ) -> Result<Box<dyn Operator>, crate::operator::Error>,
         > = match self.library.load_symbol(b"new") {
             Ok(v) => v,
             Err(e) => {
@@ -43,7 +50,13 @@ impl OperatorPlugin {
                 Err(e)?
             }
         };
-        Ok(constructor(id).map_err(|e| {
+
+        let op_id = id.map(|s| CString::new(s).unwrap());
+        let id_ptr = op_id.as_ref().map_or(std::ptr::null(), |s| s.as_ptr());
+
+        let event_tx_ptr: *const std::ffi::c_void =
+            &event_tx as *const _ as *const std::ffi::c_void;
+        Ok(constructor(id_ptr, event_tx_ptr).map_err(|e| {
             error!("{:?}", e);
             Error::Other(e.to_string())
         })?)
