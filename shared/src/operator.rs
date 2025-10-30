1a7b8eb3a7454f8e71dbae75062dac03d0a216fd
Meph1sto666 <phentragon@gmx.de>
2025-10-30 16:59:31 +0100

    feat(operator)!: finctions improved error handling

--- shared/src/operator.rs
+++ shared/src/operator.rs
@@ -1,16 +1,21 @@
+use crate::ipc::Response;
+
 pub trait Operator: std::fmt::Debug + Send + Sync {
     fn render(&mut self, ctx: &eframe::egui::Context, ui: &mut eframe::egui::Ui);
     fn id(&self) -> String;
-    fn start_animation(&mut self, anim: &str);
+    fn start_animation(&mut self, anim: &str) -> Result<(), Error>;
     fn update_animation(&mut self, ctx: &eframe::egui::Context);
     fn load_textures(&mut self, ctx: &eframe::egui::Context);
-    fn event_handler(&mut self, event: crate::events::Event);
+    fn event_handler(&mut self, event: crate::events::Event) -> Result<Response, Error>;
+    fn infos(&self) -> String;
 }
 
 #[derive(Debug)]
 #[non_exhaustive]
 pub enum Error {
     SkinError(crate::skin::Error),
+    InvalidOperatorId(String),
+    Other(String),
 }
 
 impl From<crate::skin::Error> for Error {
@@ -23,6 +28,8 @@ impl std::fmt::Display for Error {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
             Error::SkinError(error) => write!(f, "{}", error),
+            Error::InvalidOperatorId(error) => write!(f, "{}", error),
+            Error::Other(error) => write!(f, "{}", error),
         }
     }
 }
