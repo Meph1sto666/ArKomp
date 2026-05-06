963835f43faa6b2c78eb77daa7353095252f08e1
Meph1sto666 <phentragon@gmx.de>
2026-05-06 14:49:21 +0200

    fix: replaced AppState update with ui as required by eframe

--- src/ui/mod.rs
+++ src/ui/mod.rs
@@ -2,7 +2,7 @@ use eframe::{
     NativeOptions,
     egui::{CentralPanel, Color32, Frame, ViewportBuilder},
 };
-use shared::{operator::Operator, plugin::PluginRegistry};
+use shared::{ipc::events::Event, operator::Operator, plugin::PluginRegistry};
 use std::{
     collections::HashMap,
     sync::{Arc, RwLock},
@@ -12,42 +12,79 @@ use tracing::error;
 
 #[derive(Debug)]
 pub struct AppState {
-    socket_server: super::ipc_handler::WebSocketServer,
     _server_handle: tokio::task::JoinHandle<()>,
     operators: Arc<RwLock<HashMap<String, Box<dyn Operator>>>>,
     fps: u8,
+    ui_rx: tokio::sync::mpsc::Receiver<Event>,
+    mouse_passthrough: bool,
+    debug_overlay: bool,
+    _server_tx: tokio::sync::mpsc::Sender<Event>,
 }
-
 impl AppState {
     pub fn new(_cc: &eframe::CreationContext<'_>) -> Self {
         let plug_reg = Arc::new(std::sync::RwLock::new(PluginRegistry::default()));
         let op_reg = Arc::new(std::sync::RwLock::new(HashMap::default()));
-        let web_socket_server = super::ipc_handler::WebSocketServer::new(&plug_reg, &op_reg);
-        let op_reg_copy = op_reg.clone();
+
+        let (ui_tx, ui_rx) = tokio::sync::mpsc::channel::<Event>(10);
+
+        let plug_reg_clone = plug_reg.clone();
+        let op_reg_clone = op_reg.clone();
+
+        // create the socket and run it
+        let (s_tx, mut s_rx) = tokio::sync::mpsc::channel::<Event>(10);
         let server_handle = tokio::spawn(async move {
-            let web_socket_server =
-                super::ipc_handler::WebSocketServer::new(&plug_reg, &op_reg_copy);
-            if let Err(e) = web_socket_server.run("127.0.0.1:2887").await {
+            let server =
+                super::ipc_handler::WebSocketServer::new(&plug_reg_clone, &op_reg_clone, &ui_tx);
+
+            if let Err(e) = server.run("127.0.0.1:2887", &mut s_rx).await {
                 error!("WebSocket server error: {}", e);
             }
         });
 
         Self {
-            socket_server: web_socket_server,
             _server_handle: server_handle,
             operators: op_reg,
-            fps: 30,
+            fps: 15,
+            ui_rx,
+            mouse_passthrough: true,
+            debug_overlay: true,
+            _server_tx: s_tx.clone(),
+        }
+    }
+
+    fn event_handler(&mut self, ctx: &eframe::egui::Context) {
+        while let Ok(e) = self.ui_rx.try_recv() {
+            match e {
+                Event::SetMousePassthrough(val) => {
+                    self.mouse_passthrough = val;
+                    ctx.send_viewport_cmd(eframe::egui::ViewportCommand::MousePassthrough(val));
+                }
+                Event::ShowDebugOverlay(val) => self.mouse_passthrough = val,
+                _ => todo!(),
+            }
         }
     }
 }
 
 impl eframe::App for AppState {
-    fn update(&mut self, ctx: &eframe::egui::Context, _frame: &mut eframe::Frame) {
+    fn ui(&mut self, ui: &mut eframe::egui::Ui, _frame: &mut eframe::Frame) {
+        let ctx = ui.ctx();
+        self.event_handler(ctx);
         CentralPanel::default().frame(Frame::NONE).show(ctx, |ui| {
+            // ui.input(|input| {
+            //     if let Err(e) = self.server_tx.try_send(Event::OnMouseMove {
+            //         to: "all".into(),
+            //         position: input.pointer.hover_pos().map(|f| (f.x, f.y)),
+            //     }) {
+            //         debug!("{}", e.to_string());
+            //     }
+            // });
+
             let mut operators_guard = self.operators.write().unwrap();
+
             for (_, op) in operators_guard.iter_mut() {
                 op.render(ctx, ui);
-                op.update_animation(ctx);
+                op.update_animation(ui);
             }
         });
         ctx.request_repaint_after(Duration::from_secs_f32(1.0 / self.fps as f32));
